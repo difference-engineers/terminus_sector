ef07b7cffd9275c4e211715f7151f301a6053bdd
Custodius <Custodius@Gmail.com>
2021-09-26 22:59:45 +0000

    commit

--- lib/terminus_sector.ex
+++ lib/terminus_sector.ex
@@ -10,14 +10,19 @@ defmodule TerminusSector do
 
     popcurr - The Current Population
     popmx - The Maximum Population
-    induscurr - The Current Industry Utilization
-    indusMx - The Maximum Industry
+    industcurr - The Current Industry Utilization
+    industmax - The Maximum Industry
     resourcecurr - The Current resource utilization"
     resourcemx - The maximum resource utilization
     loyaltycurr - The current population loyalty
     loyaltymx - the maximum population loyalty
     technology - The current technology level. There is no maximum.
-
+    Population:	Tech+Res/2
+    Industry:	Tech+schol/3
+    Technology:	Unlimited
+    Scholarship:	Tech+pop/3
+    Resources	Ind+tech+pop/2
+    Loyalty:	Scholarship+Tech-Population
   """
 
   def provname() do
@@ -36,16 +41,40 @@ defmodule TerminusSector do
     4
   end
 
+  def popmax() do
+    div(round(techcurr() + resourcecurr()),2)
+  end
+
   def industcurr() do
     1
+  end
+
+  def industmax() do
+    div(round(techcurr() + scholcurr()),3)
+  end
+
+  def scholcurr() do
+    3
+  end
+
+  def scholmax() do
+    div(round(techcurr() + resourcecurr()),2)
   end
 
   def resourcecurr() do
     3
   end
 
+  def resourcemax() do
+    div(round(techcurr() + popcurr() + industcurr()),3)
+  end
+
   def loyaltycurr() do
     3
+  end
+
+  def loyaltymax() do
+    div(round(techcurr() + scholcurr()),- popcurr())
   end
 
   def techcurr do
@@ -53,6 +82,6 @@ defmodule TerminusSector do
   end
 
   def report do
-    "#{provname()} is owned by #{provplayer()}, a member of the #{provfact()}. #{provname()} has a Population of #{popcurr()}, an Industry Rating of #{industcurr()}, a Resources Rating of #{resourcecurr()}, a Loyalty rating of #{loyaltycurr()} and a Technology rating of #{techcurr()}."
+    "#{provname()} is owned by #{provplayer()}, a member of the #{provfact()}. #{provname()} has a Population of #{popcurr()} with a maximum of #{popmax()}, an Industry Rating of #{industcurr()} with a maximum of #{industmax()} a Resources Rating of #{resourcecurr()} with a maximum of #{resourcecurr()} , a Loyalty rating of #{loyaltycurr()} with a maximum of #{loyaltycurr()} and a Technology rating of #{techcurr()}."
   end
 end
